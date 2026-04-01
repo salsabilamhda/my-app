1ee07008a494362c4e0c47839ec07d6da2b60b2b
salsabilamhda <salsabilamahda217@gmail.com>
2026-04-01 14:23:41 +0700

    js 12

---
+++ src/pages/api/revalidate.ts
@@ -0,0 +1,38 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+  revalidated: boolean;
+  message?: string;
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>,
+) {
+  // Mengecek apakah token di query sama dengan token di .env
+  if (req.query.token !== process.env.REVALIDATE_TOKEN) {
+    return res.status(401).json({
+      revalidated: false,
+      message: "Insert correct token",
+    });
+  }
+
+  // Mengecek apakah parameter data bernilai "produk"
+  if (req.query.data === "produk") {
+    try {
+      // Memicu pembaruan halaman /produk/static
+      await res.revalidate("/produk/static");
+      return res.status(200).json({ revalidated: true });
+    } catch (error) {
+      console.error("Error in API route:", error);
+      return res.status(500).send({ revalidated: false });
+    }
+  }
+
+  // Respon jika parameter data tidak valid
+  return res.json({
+    revalidated: false,
+    message: "Invalid query parameter. Expected 'data=produk'.",
+  });
+}
--- src/pages/produk/static.tsx
+++ src/pages/produk/static.tsx
@@ -1,5 +1,5 @@
-import TampilanProduk from "../../views/produk";
-import { ProductType } from "../../types/Product.type";
+import TampilanProduk from "@/views/produk/index"; // Mengarah ke src/views/produk/index.tsx
+import { ProductType } from "@/types/Product.type";
 
 const halamanProdukStatic = (props: { products: ProductType[] }) => {
   const { products } = props;
@@ -14,7 +14,7 @@ const halamanProdukStatic = (props: { products: ProductType[] }) => {
 export default halamanProdukStatic;
 
 export async function getStaticProps() {
-  const res = await fetch('http://127.0.0.1:3000/api/produk');
+  const res = await fetch("http://127.0.0.1:3000/api/produk");
   // const response: ProductType[] = await res.json();
   const response: { data: ProductType[] } = await res.json();
 
@@ -22,6 +22,7 @@ export async function getStaticProps() {
   return {
     props: {
       products: response.data,
-    }
-  }
+    },
+    revalidate: 10, // Revalidate data setiap 10 detik
+  };
 }
